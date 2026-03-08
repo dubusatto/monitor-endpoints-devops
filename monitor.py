a3a6e393ed4d6737d3fb94afdd11e36534f52558
dubusatto <dubusatto@gmail.com>
2026-03-08 14:47:47 -0300

    feat: cria estrutura inicial do monitor de endpoints

--- monitor.py
+++ monitor.py
@@ -1,16 +1,11 @@
 import requests
-from datetime import datetime
 
 URLS = ["https://google.com", "https://github.com"]
 
 def check_endpoints():
-    print(f"--- Verificação iniciada em: {datetime.now()} ---")
     for url in URLS:
-        try:
-            response = requests.get(url, timeout=5)
-            print(f"[OK] {url} - Status: {response.status_code}")
-        except Exception as e:
-            print(f"[ERRO] {url} - Falha: {e}")
+        response = requests.get(url)
+        print(f"Site: {url} | Status: {response.status_code}")
 
 if __name__ == "__main__":
     check_endpoints()
